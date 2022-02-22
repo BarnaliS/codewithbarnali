d15111c7bfcc50b4b24309fd5892367ef3437d58
Barnali Sarmah <bsarmah@manh.com>
2022-02-22 20:37:37 +0530

    collection

--- HelloWorld/src/com/codewithbarnali/Collections/GenericList.java
+++ HelloWorld/src/com/codewithbarnali/Collections/GenericList.java
@@ -1,4 +1,15 @@
 package com.codewithbarnali.Collections;
 
-public class GenericList {
+public class GenericList <T>{
+
+   private T[] items = (T[])new Object[10];
+   private int count = 0;
+
+    public void add(T item){
+        items[count++] = item;
+    }
+    public T get(int index){
+        return items[index];
+
+    }
 }
--- HelloWorld/src/com/codewithbarnali/Collections/Main.java
+++ HelloWorld/src/com/codewithbarnali/Collections/Main.java
@@ -1,4 +1,11 @@
 package com.codewithbarnali.Collections;
 
 public class Main {
+
+    public static void main(String[] args) {
+        var list = new GenericList<String>();
+        list.add("1");
+        list.add("10");
+        list.get(0);
+    }
 }
